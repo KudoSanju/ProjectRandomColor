175c07d491f90a206cc7c9c75b20459987926be4
Kudo Sanju <Noelle07072020@gmail.com>
2022-07-14 01:49:36 +0900

    first commit

---
+++ modules/test-boilerplate.js
@@ -0,0 +1,14 @@
+function test(actual, expected) {
+    if (_.isEqual(actual, expected)) {
+      console.log("Yay! Test PASSED.");
+    } else {
+      console.error("Test FAILED. Keep trying!");
+      console.log("    actual: ", actual);
+      console.log("  expected: ", expected);
+      console.trace();
+    }
+  }
+  
+  
+  
+  
---
+++ script.js
@@ -0,0 +1,18 @@
+'use strict';
+// please do not delete the 'use strict' line above
+
+document.getElementById('color-button').addEventListener('click', changeColor)
+                                                          //action,result//
+function changeColor() {
+  const r = Math.floor(Math.random() * 256);
+  const g = Math.floor(Math.random() * 256); 
+  const b = Math.floor(Math.random() * 256);
+ 
+ document.body.style.backgroundColor = "rgb(" + r + "," + g + ","+ b + ")";
+  
+  
+
+
+  console.log('Button clicked!'); // feel free to change/delete this line
+}
+
